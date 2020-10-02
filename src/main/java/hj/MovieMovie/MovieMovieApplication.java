156a07c407a66dfcc0b4e3a4492047d9db0d4ede
hyunjin lim <limguswlss@naver.com>
2020-10-03 01:22:11 +0900

    generated spring boot project

---
+++ src/main/java/hj/MovieMovie/MovieMovieApplication.java
@@ -0,0 +1,13 @@
+package hj.MovieMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hj/MovieMovie/MovieMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.MovieMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
